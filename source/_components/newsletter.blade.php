923c23b437e3b63ebd117e7e71a57e8342d2eb35
Daniel Pritchard <daniel@pritchard.co>
2023-05-17 16:01:48 +1200

    Update Newsletter Component

--- source/_components/newsletter.blade.php
+++ source/_components/newsletter.blade.php
@@ -1,7 +1,7 @@
 @props(['file', 'current' => false])
 @php
-	$img_src = 'assets/files/newsletters/' . $file . '_cover.png';
-	$file_src = 'assets/files/newsletters/' . $file . '.pdf';
+	$img_src = '/assets/files/newsletters/' . $file . '_cover.png';
+	$file_src = '/assets/files/newsletters/' . $file . '.pdf';
 @endphp
 
 <div class="flex {{ $current ? 'w-full' : 'w-1/4' }} px-2">
